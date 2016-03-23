b03780dcbf2c7d8d73cf96765836e6d886438a37
Вадим <guts.vadim@gmail.com>
2016-03-23 13:58:02 +0200

    getDefault()

--- entities/Language.php
+++ entities/Language.php
@@ -6,11 +6,27 @@ use yii\db\ActiveRecord;
 
 /**
  * @author Gutsulyak Vadim <guts.vadim@gmail.com>
+ *
+ * @property number id
+ * @property string name
+ * @property string lang_id
+ * @property boolean show
+ * @property boolean active
+ * @property boolean default
  */
 class Language extends ActiveRecord {
 
     public static function tableName() {
         return 'language';
+    }
+
+    /**
+     * @return Language
+     */
+    public static function getDefault() {
+        return Language::findOne([
+            'default' => true
+        ]);
     }
 
     public static function findOrDefault($languageId) {
@@ -21,4 +37,5 @@ class Language extends ActiveRecord {
         }
         return $language;
     }
+
 }
